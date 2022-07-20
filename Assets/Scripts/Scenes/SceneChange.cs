bdeec3a00ceb73bf22e38ad2defebea4f4c614c2
asmartcowmedia <asmartcowmedia@gmail.com>
2022-07-20 09:31:04 -0500

    updated the new assets for animation

--- Assets/Scripts/Scenes/SceneChange.cs
+++ Assets/Scripts/Scenes/SceneChange.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private string sceneToChangeTo;
+    [SerializeField] private DataPersistenceManager dataManager;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D col)
     {
-        
+        dataManager.SaveGame();
+
+        Debug.Log("Loading next scene: " + sceneToChangeTo);
+        SceneManager.LoadSceneAsync(sceneToChangeTo);
     }
 }
